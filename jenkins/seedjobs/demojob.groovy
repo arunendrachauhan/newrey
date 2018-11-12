9619045304c1dd81ed267954aff497e3b7653cc7
Arunendra Singh Chauhan <arunendra.chauhan@globant.com>
2018-11-12 22:01:37 +0530

    updated

--- jenkins/seedjobs/demojob.groovy
+++ jenkins/seedjobs/demojob.groovy
@@ -1,66 +1,29 @@
-def gitUrl = 'https://github.com/arunendrachauhan/newrey'
-def host = '$(hostname -I | cut -f2 -d' ')'
-createPipelineJob("MyDemotest", gitUrl, "jenkins/Jenkinsfile")
+def gitUrl = 'https://github.com/codecentric/conference-app'
 
-def createPipelineJob(def jobName, def gitUrl, def scriptPath) {
-  job("${jobName}") {
-    parameters {
-      stringParam("BRANCH", "master", "Define TAG or BRANCH to build from")
-      stringParam("REPOSITORY_URL", "http://${host}:8081/repository/maven-releases/", "Nexus Release Repository URL")
-    }
-    scm {
-      git {
-        remote {
-          url(gitUrl)
-        }
-        extensions {
-          cleanAfterCheckout()
-        }
-      }
-    }
-    triggers {
-      scm('30/H * * * *')
-      githubPush()
-    }
-    pipeline {
-      scm {
-        git {
-          remote {
-            url(gitUrl)
-            }
-          }
-        branches {
-          git {
-            name('*/master')
-            }
-          }
-        }
-        scriptPath(scriptPath)
-    }
-buildPipelineView('Demo Pipeline View') {
-    filterBuildQueue()
-    filterExecutors()
-    title('Demo')
-    displayedBuilds(5)
-    selectedJob("")
-    alwaysAllowManualTrigger()
-    refreshFrequency(60)
-}
+createPipelineJob ("MyDemo", gitUrl, "jenkins/Jenkinsfile")
+def createPipelineJob (def jobName, def gitUrl, def scriptPath){
+pipelineJob("${jobName}") {
+	description()
+	keepDependencies(false)
+	definition {
+		cpsScm {
+			scm {
+				git {
+					remote {
+						github(gitUrl, "https")
 
-listView('Demo List View') {
-    description('')
-    filterBuildQueue()
-    filterExecutors()
-    jobs {
-        regex(/*.*/)
-    }
-    columns {
-        status()
-        buildButton()
-        weather()
-        name()
-        lastSuccess()
-        lastFailure()
-        lastDuration()
-    }
+					}
+					branch("*/master")
+				}
+			}
+			scriptPath(scriptPath)
+		}
+	}
+	disabled(false)
+	configure {
+		it / 'properties' / 'com.coravy.hudson.plugins.github.GithubProjectProperty' {
+			'projectUrl'('https://github.globant.com/arunendra-chauhan/devopsquest4.git/')
+			displayName("DEMO")
+		}
+	}
 }
