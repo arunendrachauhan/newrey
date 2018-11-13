05bc6f65b05723b338dccd797d2171744884d1e0
Arunendra Singh Chauhan <arunendra.chauhan@globant.com>
2018-11-13 05:49:53 +0530

    updated

--- sonar/run.sh
+++ sonar/run.sh
@@ -4,7 +4,9 @@ set -e
 if [ "${1:0:1}" != '-' ]; then
 	exec "$@"
 fi
-
+SONARQUBE_JDBC_URL="jdbc:postgresql://sonardb:5432/sonar"
+SONARQUBE_JDBC_USERNAME="sonar"
+SONARQUBE_JDBC_PASSWORD="sonar"
 exec java -jar lib/sonar-application-$SONAR_VERSION.jar \
 	-Dsonar.log.console=true \
   -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
